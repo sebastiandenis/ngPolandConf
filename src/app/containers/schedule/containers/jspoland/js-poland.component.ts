7029c8cd34c3da4dda56da1a41b00ae7f5a3051d
Sebastian Denis <sebastian.denis@outlook.com>
2019-11-17 06:12:15 +0100

    schedule

--- src/app/containers/schedule/containers/jspoland/js-poland.component.ts
+++ src/app/containers/schedule/containers/jspoland/js-poland.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Observable, Subject } from "rxjs";
 import { EventItem } from "~/app/models/event-item.model";
 import {
     ContentfulService,
     EventItemType
 } from "~/app/services/contentful.service";
 import { AppStateFacadeService } from "~/app/services/app-state-facade.service";
-import { delay } from 'rxjs/operators';
+import { delay, takeUntil } from "rxjs/operators";
+import { IConference } from "~/app/models/conference.model";
 
 @Component({
     selector: "JsPoland",
@@ -14,9 +15,12 @@ import { delay } from 'rxjs/operators';
     templateUrl: "./js-poland.component.html",
     styleUrls: ["./js-poland.component.css"]
 })
-export class JsPolandComponent implements OnInit {
+export class JsPolandComponent implements OnInit, OnDestroy {
     events$: Observable<Array<EventItem>>;
     isLoading$: Observable<boolean>;
+    destroySubject$ = new Subject<void>();
+    conference: IConference;
+    themeApplied = false;
 
     constructor(private appStateFacade: AppStateFacadeService) {
         // Use the component constructor to inject providers.
@@ -26,9 +30,26 @@ export class JsPolandComponent implements OnInit {
     ngOnInit(): void {
         // Init your component properties here.
         this.events$ = this.appStateFacade.getEventsJsPoland().pipe(delay(600));
+
+        this.appStateFacade
+            .getCurrentConference()
+            .pipe(takeUntil(this.destroySubject$))
+            .subscribe((conf: IConference) => {
+                this.conference = conf;
+            });
+        this.appStateFacade
+            .getThemeApplied()
+            .pipe(takeUntil(this.destroySubject$))
+            .subscribe((isThemeApplied: boolean) => {
+                this.themeApplied = isThemeApplied;
+            });
     }
 
     refreshData(arg: any) {
         this.appStateFacade.initStateFromTheInternet();
+    }
+
+    ngOnDestroy(): void {
+        this.destroySubject$.next();
     }
 }
--- src/app/containers/schedule/containers/ngpoland/ng-poland.component.ts
+++ src/app/containers/schedule/containers/ngpoland/ng-poland.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Observable, Subject } from "rxjs";
 import { EventItem } from "../../../../models/event-item.model";
 import { AppStateFacadeService } from "../../../../services/app-state-facade.service";
-import { delay } from "rxjs/operators";
+import { delay, takeUntil } from "rxjs/operators";
+import { IConference } from "~/app/models/conference.model";
 
 @Component({
     selector: "NgPoland",
@@ -10,9 +11,12 @@ import { delay } from "rxjs/operators";
     templateUrl: "./ng-poland.component.html",
     styleUrls: ["./ng-poland.component.css"]
 })
-export class NgPolandComponent implements OnInit {
+export class NgPolandComponent implements OnInit, OnDestroy {
     events$: Observable<Array<EventItem>>;
     isLoading$: Observable<boolean>;
+    destroySubject$ = new Subject<void>();
+    conference: IConference;
+    themeApplied = false;
 
     constructor(private appStateFacade: AppStateFacadeService) {
         // Use the component constructor to inject providers.
@@ -22,9 +26,26 @@ export class NgPolandComponent implements OnInit {
     ngOnInit(): void {
         // Init your component properties here.
         this.events$ = this.appStateFacade.getEventsNgPoland().pipe(delay(400));
+
+        this.appStateFacade
+            .getCurrentConference()
+            .pipe(takeUntil(this.destroySubject$))
+            .subscribe((conf: IConference) => {
+                this.conference = conf;
+            });
+        this.appStateFacade
+            .getThemeApplied()
+            .pipe(takeUntil(this.destroySubject$))
+            .subscribe((isThemeApplied: boolean) => {
+                this.themeApplied = isThemeApplied;
+            });
     }
 
     refreshData(arg: any) {
         this.appStateFacade.initStateFromTheInternet();
+    }
+
+    ngOnDestroy(): void {
+        this.destroySubject$.next();
     }
 }
